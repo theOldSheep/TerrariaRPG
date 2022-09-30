1a44787c808a75a624ec248a65ab7357dba4071c
theOldSheep <sunnymengyangliu@gmail.com>
2022-09-29 21:01:19 -0600

    worldgen continuous fix

--- 泰拉瑞亚RPG灾厄/[绵羊服]泰拉瑞亚RPG灾厄服务端/plugin_development/TerrariaHelper/src/main/java/terraria/worldgen/RandomGenerator.java
+++ 泰拉瑞亚RPG灾厄/[绵羊服]泰拉瑞亚RPG灾厄服务端/plugin_development/TerrariaHelper/src/main/java/terraria/worldgen/RandomGenerator.java
@@ -8,13 +8,13 @@ import java.util.Random;
 public class RandomGenerator {
     public static double getRandom(long worldSeed, int x, int z) {
         return getRandomGenerator(worldSeed, x, z).nextDouble();
-//        return getRandomGenerator(worldSeed, x, z).nextDouble();
     }
     public static Random getRandomGenerator(long worldSeed, int x, int z) {
-        return new Random(worldSeed + x * 11451L + z * 41919L);
+        return new Random(worldSeed + x * 11451L + z * 41919L + (long) x * z);
 //        return new Random(worldSeed + new Random(new Random(x).nextLong() + z * 1145).nextLong());
     }
     public static double getRandomByPerlinNoise(long worldSeed, int x, int z) {
+        // as perlin noise uses trig functions this is a lot slower than random generator.
         return PerlinNoiseGenerator.getNoise(worldSeed, x * 114514, z * 1919810);
     }
 }
