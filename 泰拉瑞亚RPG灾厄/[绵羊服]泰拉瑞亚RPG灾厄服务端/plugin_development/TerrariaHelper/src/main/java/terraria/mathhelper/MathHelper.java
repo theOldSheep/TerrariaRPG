e772dede3e57effdd8859945382c0fcb4d2d0137
theOldSheep <sunnymengyangliu@gmail.com>
2022-10-06 00:07:24 -0600

    biomegen bugfix

---
+++ 泰拉瑞亚RPG灾厄/[绵羊服]泰拉瑞亚RPG灾厄服务端/plugin_development/TerrariaHelper/src/main/java/terraria/mathhelper/MathHelper.java
@@ -0,0 +1,9 @@
+package terraria.mathhelper;
+
+public class MathHelper {
+    public static int betterFloorDivision(int dividend, int divisor) {
+        int result = dividend / divisor;
+        if (dividend < 0 && dividend % divisor != 0) result --;
+        return result;
+    }
+}
