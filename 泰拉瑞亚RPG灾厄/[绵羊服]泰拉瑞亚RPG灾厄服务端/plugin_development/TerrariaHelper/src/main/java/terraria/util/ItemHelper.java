89e36810d42bfd3a00388b9714fd3cda787c8743
theOldSheep <sunnymengyangliu@gmail.com>
2022-10-30 12:07:03 -0600

    itemutil null argument handling

--- 泰拉瑞亚RPG灾厄/[绵羊服]泰拉瑞亚RPG灾厄服务端/plugin_development/TerrariaHelper/src/main/java/terraria/util/ItemHelper.java
+++ 泰拉瑞亚RPG灾厄/[绵羊服]泰拉瑞亚RPG灾厄服务端/plugin_development/TerrariaHelper/src/main/java/terraria/util/ItemHelper.java
@@ -4,7 +4,7 @@ import org.bukkit.inventory.ItemStack;
 
 public class ItemHelper {
     public static String[] splitItemName(String itemName) {
-        if (itemName == null) return null;
+        if (itemName == null) return new String[]{"", ""};
         itemName = GenericHelper.trimText(itemName);
         if (itemName.contains("的 ")) {
             return itemName.split("的 ");
@@ -12,11 +12,12 @@ public class ItemHelper {
         return new String[]{"", itemName};
     }
     public static String[] splitItemName(ItemStack item) {
+        if (item == null) return new String[]{"", ""};
         return splitItemName(item.getItemMeta().getDisplayName());
     }
     public static int getWorth(String name) {
+        if (name == null) name = "";
         String[] nameInfo = splitItemName(name);
-        if (nameInfo == null) return 0;
         int worth = YmlHelper.getFile("plugins/Data/items.yml").getInt(nameInfo[1] + ".worth", 0);
         double worthMulti = YmlHelper.getFile("plugins/Data/prefix.yml").getDouble(
                 "prefixInfo." + nameInfo[0] + ".priceMultiplier", 1);
@@ -24,6 +25,7 @@ public class ItemHelper {
         return (worth / 100) * 100;
     }
     public static int getWorth(ItemStack item) {
+        if (item == null) return 0;
         return getWorth(item.getItemMeta().getDisplayName());
     }
 
